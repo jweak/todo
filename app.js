0c088c6d1afbaf595f0c933a9c0fa5644ae18edf
Juho Heikka <juho.heikka@gmail.com>
2014-05-27 19:24:42 +0300

    Add event handling. Adding, deleting and toggling for todos.

--- app.js
+++ app.js
@@ -9,71 +9,203 @@ if (document.readyState == "complete") {
   }
 }
 
-function State() {
+function Task() {
   this.todos = [];
+  this.todoIdCounter = 0;
+  this.listeners = [];
 };
 
-State.prototype.addTodo = function(todo) {
+Task.prototype.add = function(todo) {
+  todo.id = "todo-" + this.todoIdCounter++;
   this.todos.push(todo);
+  this.onChange();
+};
+
+Task.prototype.remove = function(id) {
+  var found = this.find(id);
+  if (found) {
+    var foundIndex = this.todos.indexOf(found);
+    this.todos.splice(foundIndex, 1);
+    this.onChange();
+  }
+};
+
+Task.prototype.toggle = function(id) {
+  var found = this.find(id);
+  if (found) {
+    found.done = !found.done;
+    this.onChange();
+  }
+};
+
+Task.prototype.find = function(id) {
+  var filteredTodos = this.todos.filter(function(todo) {
+    return todo.id === id;
+  });
+  var found = filteredTodos[0];
+  return found;
 }
 
+Task.prototype.findAll = function() {
+  return this.todos;
+}
+
+Task.prototype.findOpen = function() {
+  return this.todos.filter(function(todo) {
+    return !todo.done;
+  });
+}
+
+Task.prototype.addListener = function(listener) {
+  this.listeners.push(listener);
+};
+
+Task.prototype.onChange = function() {
+  this.listeners.forEach(function(listener) {
+    listener();
+  });
+};
 
 function TodoApp(elementId) {
   this.element = document.querySelector("." + elementId);
 
+  this.tasks = new Task();
+  this.tasks.add({ 
+    text: "Clean kitchen",
+    done: true
+  });
+  this.tasks.add({ 
+    text: "Pick up children from kindergarten",
+    done: false
+  });
 
-  this.state = new State();
-  this.state.addTodo(this.getTodo("Clean kitchen", true));
-  this.state.addTodo(this.getTodo("Pick up children from kindergarten"));
+  this.tasks.addListener(this.onStateChange.bind(this));
 
-  this.listView = new TodoListView(this.state.todos);
+  this.listView = new TodoListView();
+  this.listView.addDeleteListener(this.onDelete.bind(this));
+  this.listView.addToggleListener(this.onToggle.bind(this));
   this.textBox = new TextBox();
-}
+  this.textBox.addListener(this.onNewTodo.bind(this));
+};
 
-TodoApp.prototype.getTodo = function(todoStr, done) {
-  return {
+TodoApp.prototype.onDelete = function(todoId) {
+  this.tasks.remove(todoId);
+};
+
+TodoApp.prototype.onToggle = function(todoId) {
+  this.tasks.toggle(todoId);
+};
+
+TodoApp.prototype.onNewTodo = function(todoStr) {
+  this.tasks.add({
     text: todoStr,
-    done: done || false
-  }
-}
+    done: false
+  });
+};
+
+TodoApp.prototype.onStateChange = function() {
+  this.listView.render(this.tasks.findAll());
+};
 
 TodoApp.prototype.render = function() {
-  this.element.appendChild(this.listView.render());
+  this.element.appendChild(this.listView.render(this.tasks.findAll()));
   this.element.appendChild(this.textBox.render());
 };
 
 function TodoListView(todos) {
   this.element = document.createElement("ul");
   this.element.className = "todo-list"
-  this.todos = todos;
   this.template = 
-  '<li class="todo-list-item light-border-bottom __DONE__">\
-    <span class="dot"></span>\
-    <span class="text light-border-left">__TEXT__</span>\
-    <span class="delete"></span>\
-  </li>';
+    '<li class="todo-list-item light-border-bottom __DONE__" id="__ID__">\
+      <span class="dot"></span>\
+      <span class="text light-border-left">__TEXT__</span>\
+      <span class="delete"></span>\
+    </li>';
+  this.deleteListeners = [];
+  this.toggleListeners = [];
+  this.clickListener = this.onClick.bind(this);
 };
 
-TodoListView.prototype.render = function() {
-  var template = this.template;
-  var innerHTML = this.todos.map(function(todo) {
-    return template.replace("__TEXT__", todo.text).replace("__DONE__", todo.done ? "done" : "");
-  }).reduce(function(a, b) {
-    return a + b;
-  });
-  this.element.innerHTML = innerHTML;
+TodoListView.prototype.render = function(todos) {
+  this.element.removeEventListener("click", this.clickListener, false);
+  
+  var html = this.generateHTML(todos);
+  this.element.innerHTML = html;
+
+  this.element.addEventListener("click", this.clickListener, false);
   return this.element;
+};
+
+TodoListView.prototype.generateHTML = function(todos) {
+  var template = this.template;
+  var html =  todos.map(function(todo) {
+    return template.replace("__TEXT__", todo.text)
+          .replace("__DONE__", todo.done ? "done" : "")
+          .replace("__ID__", todo.id);
+  }).reduce(function(previousValue, currentValue) {
+    return previousValue + currentValue;
+  }, "");
+
+  return html;
+}
+
+TodoListView.prototype.addDeleteListener = function(listener) {
+  this.deleteListeners.push(listener);
+};
+
+TodoListView.prototype.addToggleListener = function(listener) {
+  this.toggleListeners.push(listener);
+};
+
+TodoListView.prototype.onClick = function(event) {
+  var target = event.srcElement;
+  var id = target.parentElement.id;
+  if (target.classList.contains("delete")) {
+    this.deleteListeners.forEach(function(listener) {
+      listener(id);
+    });
+  } else if (target.classList.contains("dot")) {
+    this.toggleListeners.forEach(function(listener) {
+      listener(id);
+    });
+  }
 };
 
 function TextBox(element) {
+  this.listeners = [];
   this.element = document.createElement("div");
   this.element.className = "textbox light-border-bottom";
-}
+};
+
+TextBox.prototype.addListener = function(listener) {
+  this.listeners.push(listener);
+};
 
 TextBox.prototype.render = function() {
+  var input = this.getInputElement();
+  if (input) {
+    input.removeEventListener("keypress", this.onKeyPress.bind(this), false);
+  }
   this.element.innerHTML = '<div class="light-border-left margin-left-column"><input type="text" placeholder="Add a new task..."></input></div>';
+  this.getInputElement().addEventListener("keypress", this.onKeyPress.bind(this), false);
   return this.element;
-}
+};
+
+TextBox.prototype.getInputElement = function() {
+  return this.element.querySelector("input");
+};
+
+TextBox.prototype.onKeyPress = function(event) {
+  if (event.keyCode === 13) {
+    var inputString = event.target.value;
+    if (inputString !== "") {
+      this.listeners.forEach(function(listener) {
+        listener(inputString);
+      });
+      event.target.value = "";
+    }
+  }
+};
 
 function TodoStart() {
   var app = new TodoApp("app-container");
